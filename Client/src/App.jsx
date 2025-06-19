826688aa5a72c970778852c487cb3d2c12db3a61
Rhushikeshnandodkar <rhushikeshnandodkar2003@gmail.com>
2025-06-19 12:21:44 +0530

    register problem solved

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RegisterUser from "./components/pages/registerUser";
+import RegisterUser from "./components/pages/RegisterUser";
 import "./index.css"; // Ensure this points to your global CSS file
 import LoginUser from "./components/pages/LoginUser";
 import Dashboard from "./components/pages/Dashboard";
